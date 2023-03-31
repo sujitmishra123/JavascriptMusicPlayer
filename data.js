deb30341faaefed57a7b031b18da984d8fdb471f
sujit kumar <sujitmishra995@gmail.com>
2023-03-31 16:55:14 +0530

    upload by me

---
+++ data.js
@@ -0,0 +1,27 @@
+let songs = [
+    {
+        name: 'song1',
+        path: './song1.mp3',
+        artist: 'artist 1',
+        cover: './image1.png'
+    },
+    {
+        name: 'song2',
+        path: './song2.mp3',
+        artist: 'artist 2',
+        cover: './image2.png'
+
+    },
+    {
+        name: 'song3',
+        path: './song3.mp3',
+        artist: 'artist 3',
+        cover: './image3.png'
+    },
+    {
+        name: 'song4',
+        path: './song4.mp3',
+        artist: 'artist 4',
+        cover: './image4.png'
+    }
+];
